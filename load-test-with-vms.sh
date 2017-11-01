8b5a0edc01dccd33b2bfe3606c7e006722e281e2
Roi Dayan <roid@mellanox.com>
2017-11-01 12:24:36 +0200

    Update load-test-with-vms.sh to call load-test.sh
    Removed duplicate code that load-test.sh is already doing.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- load-test-with-vms.sh
+++ load-test-with-vms.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
 set -e
-
-HOST=`hostname -s`
-SHORT=`hostname -s | sed 's/-r-vrt-//'`
-
-echo "HOST $HOST"
-echo "SHORT $SHORT"
-
-SCRIPT="load-${SHORT}.sh"
-
-echo "SCRIPT $SCRIPT"
-
 DIR=$(cd `dirname $0` && pwd)
 
 export WITH_VMS=1
-$DIR/$SCRIPT
+$DIR/load-test.sh
