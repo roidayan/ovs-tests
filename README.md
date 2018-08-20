19cef2fc5102b04208248b4ab74f4de008160924
Raed Salem <raeds@mellanox.com>
2018-08-20 16:25:01 +0300

    Add support for developers tests run and add
    Files added:
    - Add test-all-dev.py: a wrapper script to run all existing tests
    - Add gen_test_xml.py: a utility script to generate xml configuration file
                          for new test
    - README.md: a readme file that covers tests running and adding process
    - tests_conf folder added which includes initial xml configuration files
      for existing tests
    
    Change-Id: I653f6c39fbcca43d7d679b58bc8a3d9ac39db657
    Signed-off-by: Raed Salem <raeds@mellanox.com>

--- README.md
+++ README.md
@@ -1,16 +1,143 @@
-# OVS Tests
+OVS developer’s tests brief overview
 
-A collection of tests for the openvswitch TC offload project.  
-Each test has a prefix of "test-". second word is for grouping related tests.
-test-all.py is a wrapper to easily run all tests.                            
+August 2018
 
-Before running tests need to create a config file. see examples that already
-exists. then export it to var CONFIG.                                       
-The config file needs to exists in current directory or with the tests.     
+Run tests steps
+===============
 
-Example to run a test:
+1.  install required packages:
 
-```
-export CONFIG=config_dev139.sh
-~/ovs-tests/test-tc-insert-rules.sh
-```
+    1.  upstream kernel under test (branch net-next):
+
+    2.  openvswitch:
+        if not altered use (https://github.com/openvswitch/ovs.git branch
+        master), installation instructions can be found at
+        http://docs.openvswitch.org/en/latest/intro/install/
+
+    3.  iproute2: if not altered use
+        (https://github.com/ndev2/iproute2-next.git branch master)
+
+    4.  install iperf latest version
+
+    5.  install iperf3 latest version
+
+    6.  make sure python scapy library installed as some tests uses it,
+        python2-scapy.noarch or python34-scapy.noarch
+
+2.  clone developers tests package:
+    git clone ssh://<user>@l-gerrit.mtl.labs.mlnx:29418/asap_dev_reg
+    when <user> is the gerrit user name (can also use the clone command from
+    gerrit project general page)
+
+3.  for the NIC under test need to define a custom configurations file (at
+    asap_dev_reg project root directory), which includes relevant NIC network
+    interfaces data (PF0, PF1, VF0,VF1,REP0,REP1):
+
+    1.  Nic type should be dual port NIC ConnectX-5 in Ethernet mode when:
+
+        1.  PF0 is the network interface of the NIC’s first physical function
+
+        2.  PF1 is the network interface of the NIC’s second physical function
+            (the second port)
+
+        3.  VF0 is the network interface of the first virtual function created
+            on PF0
+
+        4.  VF1 is the network interface of the second virtual function created
+            on PF0
+
+        5.  REP0 is the network interface of the first representor created when
+            switched to switchdev mode on PF0
+
+        6.  REP1 is the network interface of the second representor created when
+            switched to switchdev mode on PF0
+
+    2.  example configuration file needed:
+        NIC=<PF0>
+        NIC2=<PF1>
+        VF=<VF0>
+        VF1=$VF
+        VF2=<VF1>
+        REP=<REP0>
+        REP2=<REP1>
+
+4.  to run tests (at asap_dev_reg project root directory):
+
+    1.  Make sure sriov is enabled on the NIC and that the number
+        of vfs allowed is at least two
+
+    2.  export configuration file:
+        export CONFIG=<file from 3>
+
+    3.  run ./test-all-dev.py
+
+
+Notes
+=====
+
+1. For Dell setups need to add a grub parameter “biosdevname=0” to
+   disable consistent network device naming.
+
+Add new test steps
+==================
+
+1.  clone developers tests package:
+    git clone ssh://<user>@l-gerrit.mtl.labs.mlnx:29418/asap_dev_reg
+    when <user> is the gerrit user name (can also use the clone command from
+    gerrit project general page)
+
+2.  add relevant test with test naming convention “test-<…>.sh”
+
+3.  add a matching xml configuration file under “tests_conf” directory with
+    naming convention <test_name from 2>.xml , the xml will include the
+    following data (see existing XML file for exact structure):
+
+    1.  Test info section:
+
+        1.  name
+
+        2.  description
+
+        3.  owner
+
+    2.  timeout for the test
+
+    3.  tags relevant to test (tags could be passed to tests-all-dev script
+        using --exclude_tag argument to exclude all tests with these tags
+        defined from executing)
+
+    4.  ignore section that could have one or more of the following sections:
+
+        1.  bug: to input known RM bug number related to this tests
+
+        2.  IgnoreByTestAll: to ignore still unsupported tests for upstream or
+            to mark a test that is currently support to it still not merged to
+            the current upstream branch and so on.
+
+
+Notes
+=====
+
+1.  a lot of common utility functions for example get Mellanox interfaces,
+    unbind/bind vfs and many other utility functions are part of “common.sh”
+    script file (can be found at asap_dev_reg root directory) recommended to add
+    the following lines at the top of each new test script to load common
+    functions into the current shell script:
+    my_dir="\$(dirname "\$0")"
+    . \$my_dir/common.sh
+
+2. there is a utility script “gen_test_xml.py” provided to automate step "3."
+   when adding a new test:
+        usage: gen_test_xml.py [-h] --fname FNAME --desc DESC --owner OWNER
+                       [--tout TOUT] --tag TAG [--ignore_test] [--bug BUG]
+
+	optional arguments:
+		-h, --help     show this help message and exit
+		--fname FNAME  Pass the file name of the test (Mandatory)
+		--desc DESC    Pass a small description of the test (Mandatory)
+		--owner OWNER  test owner name
+		--tout TOUT    time out for the test (default 1000)
+		--tag TAG      associate a tag(s) with this test (Mandatory)
+		--ignore_test  ignore this test when running test-all-dev.py
+		--bug BUG      associate a bug(s) with this test
+
---
+++ test-all-dev.py
@@ -0,0 +1,286 @@
+#!/usr/bin/python
+
+import os
+import re
+import sys
+import argparse
+import subprocess, threading
+from glob import glob
+from lxml import etree
+from tempfile import mkdtemp
+from mlxredmine import MlxRedmine
+
+
+MYNAME = os.path.basename(__file__)
+MYDIR = os.path.abspath(os.path.dirname(__file__))
+LOGDIR = mkdtemp(prefix='log')
+DEST_DIR = os.path.join(MYDIR,"tests_conf")
+
+TESTS = sorted(glob(MYDIR + '/test-*.sh'))
+IGNORE_TESTS = [MYNAME,"test-all.py"]
+SKIP_TESTS = {}
+SKIP_TAGS = []
+
+COLOURS = {
+    "black": 30,
+    "red": 31,
+    "green": 32,
+    "yellow": 33,
+    "blue": 34,
+    "magenta": 25,
+    "cyan": 36,
+    "light-gray": 37,
+    "dark-gray": 90,
+    "light-red": 91,
+    "light-green": 92,
+    "light-yellow": 93,
+    "light-blue": 94,
+    "light-magenta": 95,
+    "light-cyan": 96,
+    "white": 97,
+}
+
+
+class ExecCmdFailed(Exception):
+    def __init__(self, cmd, rc, logname):
+        self._cmd = cmd
+        self._rc = rc
+        self._logname = logname
+
+    def __str__(self):
+        return self._logname
+
+class Command(object):
+    def __init__(self, cmd,timeout):
+        self.cmd = cmd
+        self.timeout = timeout
+        self.process = None
+        self.output = None
+        self.logname = None
+
+    def run(self):
+        def target():
+            #print 'Thread started'
+            self.logname = os.path.join(LOGDIR, os.path.basename(self.cmd)+'.log')
+            with open(self.logname, 'w') as f1:
+                # piping stdout to file seems to miss stderr msgs so we use pipe
+                # and write to file at the end.
+                self.process = subprocess.Popen(self.cmd, shell=True, stdout=subprocess.PIPE,
+                                                stderr=subprocess.STDOUT, close_fds=True)
+                self.out = self.process.communicate()
+                f1.write(self.out[0])
+            #print 'Thread finished'
+            return
+
+        try:
+            thread = threading.Thread(target=target)
+            thread.start()
+
+            thread.join(int(float(self.timeout)))
+            if thread.is_alive():
+               self.process.terminate()
+               thread.join()
+               status = "Test Timeout (> %s sec) - %s "%(self.timeout,self.logname)
+               raise ExecCmdFailed(self.cmd, self.process.returncode, status)
+        except KeyboardInterrupt:
+               self.process.terminate()
+               thread.join(0)
+               print 'Interrupted'
+               sys.exit(1)
+        status = self.out[0].splitlines()[-1].strip()
+        status = strip_color(status)
+        if self.process.returncode:
+           status = "(%s) %s" % (status, self.logname)
+           raise ExecCmdFailed(self.cmd, self.process.returncode, status)
+        return status
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='verbose output')
+    parser.add_argument('--stop', '-s', action='store_true',
+                        help='stop on first error')
+    parser.add_argument('--dry', '-d', action='store_true',
+                        help='not to actually run the test')
+    parser.add_argument('--from_test', '-f',
+                        help='start from test')
+    parser.add_argument('--exclude', '-e', action='append',
+                        help='exclude test')
+    parser.add_argument('--glob', '-g',
+                        help='glob of tests')
+    parser.add_argument('--parm', '-p',
+                        help='Pass parm to each test')
+    parser.add_argument('--exclude_tag', action='append',  
+                        help='exclude all tests with tag')
+    args = parser.parse_args()
+    return args
+
+# add timeout
+def run_test(cmd):
+    logname = os.path.join(LOGDIR, os.path.basename(cmd)+'.log')
+    with open(logname, 'w') as f1:
+        # piping stdout to file seems to miss stderr msgs so we use pipe
+        # and write to file at the end.
+        subp = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT, close_fds=True)
+        out = subp.communicate()
+        f1.write(out[0])
+
+    status = out[0].splitlines()[-1].strip()
+    status = strip_color(status)
+
+    if subp.returncode:
+        status = "(%s) %s" % (status, logname)
+        raise ExecCmdFailed(cmd, subp.returncode, status)
+
+    return status
+
+
+def deco(line, color):
+    return "\033[%dm%s\033[0m" % (COLOURS[color], line)
+
+
+def strip_color(line):
+    return re.sub("\033\[[0-9 ;]*m", '', line)
+
+
+def print_result(res, out):
+    res_color = {
+        'SKIP': 'yellow',
+        'TEST PASSED': 'green',
+        'OK': 'green',
+        'DRY': 'yellow',
+        'FAILED': 'red',
+        'IGNORED': 'yellow',
+    }
+    color = res_color.get(res, 'yellow')
+    cres = deco(res, color)
+    if out:
+        if res == 'SKIP':
+            out = ' (%s)' % out
+        else:
+            out = ' %s' % out
+        cres += deco(out, color)
+    print cres
+
+
+def glob_tests(args, tests):
+    if not args.glob:
+        return
+    from fnmatch import fnmatch
+    for test in tests[:]:
+        name = os.path.basename(test)
+        if not fnmatch(name, args.glob):
+            tests.remove(test)
+
+
+def update_skip_according_to_xml():
+    global SKIP_TESTS
+    SKIP_TESTS = {}
+    rm = MlxRedmine()
+    print "Check redmine for open issues XML"
+    for t in TESTS:
+       test_name = os.path.basename(t)
+       test_conf_f = "{0}".format(os.path.splitext(test_name)[0]+".xml")
+       test_conf_f = os.path.join(DEST_DIR,test_conf_f)
+       if not os.path.exists(test_conf_f):
+          SKIP_TESTS[test_name] = "noneupstream"
+          continue
+       tree = etree.parse(test_conf_f) 
+       for bug_en in tree.findall("./ignore/bug"):
+           if bug_en.text is not None:
+              task = rm.get_issue(bug_en.text)
+              if rm.is_issue_open(task):
+                 SKIP_TESTS[test_name] = "RM #%s: %s" % (bug_en.text, task['subject'])
+              print '.',
+              sys.stdout.flush()
+       if t in SKIP_TESTS:
+          continue
+       if tree.find("./ignore/IgnoreByTestAll") is not None:
+          SKIP_TESTS[test_name] = "IGNORE_FROM_TEST_ALL"
+       for tag in tree.findall("./tags/tag"):
+           if tag.text in SKIP_TAGS:
+              SKIP_TESTS[test_name] = "Skipped by tag: %s" % (tag.text)
+    print
+  
+def should_ignore_test(name):
+    if name in IGNORE_TESTS or name in ' '.join(IGNORE_TESTS):
+        return True
+    else:
+        return False
+
+# TBD: parse xml for 
+def get_test_timeout(test_path):
+    test_name = os.path.basename(test_path)
+    test_conf_f = "{0}".format(os.path.splitext(test_name)[0]+".xml")
+    test_conf_f = os.path.join(DEST_DIR,test_conf_f)
+    tree = etree.parse(test_conf_f)
+    return tree.find("./tout").text
+
+def main():
+    args = parse_args()
+    ignore = False
+    if args.from_test:
+        ignore = True
+    if args.exclude:
+        IGNORE_TESTS.extend(args.exclude)
+    if args.exclude_tag:
+        SKIP_TAGS.extend(args.exclude_tag)
+    glob_tests(args, TESTS)
+
+    print "Log dir: " + LOGDIR
+    try:
+        update_skip_according_to_xml()
+    except KeyboardInterrupt:
+        print 'Interrupted'
+        sys.exit(1)
+
+    for test in TESTS:
+        name = os.path.basename(test)
+        if name == MYNAME:
+            continue
+        if ignore:
+            if args.from_test != name:
+                continue
+            ignore = False
+        if name in SKIP_TESTS and SKIP_TESTS[name]=="noneupstream":
+           continue
+        print "Test: %-60s  " % deco(name, 'blue'),
+        sys.stdout.flush()
+
+        failed = False
+        res = 'OK'
+        out = ''
+
+        if should_ignore_test(name):
+            res = 'IGNORED'
+        elif name in SKIP_TESTS:
+            res = 'SKIP'
+            out = SKIP_TESTS[name]
+        elif args.dry:
+            res = 'DRY'
+        else:
+            try:
+                cmd = test
+                if args.parm:
+                    cmd += ' ' + args.parm
+                cmdRun = Command(cmd,get_test_timeout(test))
+                res = cmdRun.run()
+            except ExecCmdFailed, e:
+                failed = True
+                res = 'FAILED'
+                out = str(e)
+            except KeyboardInterrupt:
+                print 'Interrupted'
+                sys.exit(1)
+
+        print_result(res, out)
+
+        if args.stop and failed:
+            sys.exit(1)
+    # end test loop
+
+
+if __name__ == "__main__":
+    main()
