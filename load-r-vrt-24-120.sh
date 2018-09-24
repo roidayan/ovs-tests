6fb75db7839039a721b8f4c5e05c357a2b0b3391
Ziyad Atiyyeh <ziyadat@mellanox.com>
2018-09-24 06:17:13 +0000

    load-r-vrt-24-120.sh: Fix r-vrt-24-120 vm's name in load script file
    Change-Id: Ia6764a377bae9ab21157f5ebe40cf2f69465c70f
    Signed-off-by: Ziyad Atiyyeh <ziyadat@mellanox.com>

--- load-r-vrt-24-120.sh
+++ load-r-vrt-24-120.sh
@@ -111,7 +111,7 @@ function stop_vms() {
 
 function start_vms() {
     echo "Start vms"
-    for ((i=0; i< ${#vms[@]}; i++)) do virsh -q start ${hv}-${vms[i]}-RH-7.2 ; done
+    for ((i=0; i< ${#vms[@]}; i++)) do virsh -q start ${hv}-${vms[i]}-CentOS-7.2 ; done
 }
 
 function wait_vms() {
