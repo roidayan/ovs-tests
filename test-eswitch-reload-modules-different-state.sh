2f85dc43d15cc7e64be621470764c25de997751d
Ziyad Atiyyeh <ziyadat@mellanox.com>
2018-11-04 10:45:35 +0200

    Enable reloading mlx5_ib for upstream setup
    Change-Id: Ia81f87a5abad474f1ab51f1d95815ea020907996
    Signed-off-by: Ziyad Atiyyeh <ziyadat@mellanox.com>

--- test-eswitch-reload-modules-different-state.sh
+++ test-eswitch-reload-modules-different-state.sh
@@ -53,9 +53,8 @@ function reload_mlx5() {
     if [ "$devlink_compat" = 1 ]; then
 	service openibd force-restart
     else
-        # Disable mlx5_ib reload since it is not included with JD kernel
-        modprobe -r mlx5_core || fail "Failed to unload modules"
-        modprobe -a mlx5_core || fail "Failed to load modules"
+        modprobe -r mlx5_ib mlx5_core || fail "Failed to unload modules"
+        modprobe -a mlx5_core mlx5_ib || fail "Failed to load modules"
     fi
 
     set_macs
