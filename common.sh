44f1f4f0285c4abf050cd159207ff0fb6d9487f6
Roi Dayan <roid@mellanox.com>
2018-12-06 18:19:14 +0200

    common.sh: Minimize unbind/bind vfs prints
    When testing with lots of vfs we were flooded with lots of
    prints which we don't really need.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- common.sh
+++ common.sh
@@ -425,10 +425,10 @@ function set_macs() {
 
 function unbind_vfs() {
     local nic=${1:-$NIC}
+    echo "unbind vfs of $nic"
     for i in `ls -1d /sys/class/net/$nic/device/virt*`; do
         vfpci=$(basename `readlink $i`)
         if [ -e /sys/bus/pci/drivers/mlx5_core/$vfpci ]; then
-            echo "unbind $vfpci"
             echo $vfpci > /sys/bus/pci/drivers/mlx5_core/unbind
         fi
     done
@@ -436,10 +436,10 @@ function unbind_vfs() {
 
 function bind_vfs() {
     local nic=${1:-$NIC}
+    echo "bind vfs of $vfpci"
     for i in `ls -1d /sys/class/net/$nic/device/virt*`; do
         vfpci=$(basename `readlink $i`)
         if [ ! -e /sys/bus/pci/drivers/mlx5_core/$vfpci ]; then
-            echo "bind vf $vfpci"
             echo $vfpci > /sys/bus/pci/drivers/mlx5_core/bind
         fi
     done
