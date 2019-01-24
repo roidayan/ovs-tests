b1c460a5c0a3978b214018dfb629032388d1bd91
Ziyad Atiyyeh <ziyadat@mellanox.com>
2019-01-24 09:58:38 +0200

    Add support for cx4lx on r-vrt-24-180
    Change-Id: I5e3b9200e6f5b3afb35ba15baec99ffe80991673
    Signed-off-by: Ziyad Atiyyeh <ziyadat@mellanox.com>

---
+++ config_r-vrt-24-180_cx4lx.sh
@@ -0,0 +1,7 @@
+NIC=ens2f0
+NIC2=ens2f1
+VF=ens2f0v0
+VF1=$VF
+VF2=ens2f0v1
+REP=ens2f0_0
+REP2=ens2f0_1
--- config_r-vrt-24-180_cx5.sh
+++ config_r-vrt-24-180_cx5.sh
@@ -1,7 +1,7 @@
 NIC=ens1f0
 NIC2=ens1f1
-VF=ens1f2
+VF=ens1f0v0
 VF1=$VF
-VF2=ens1f3
+VF2=ens1f0v1
 REP=ens1f0_0
 REP2=ens1f0_1
