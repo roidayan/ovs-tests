roidayan/ovs-tests

commit ff4588d10564e24584d1314e7f29fd04a0f0aab5

tc_tests_common: Fix checking input_file
Empty input_file cause an issue. verify it has data.

Change-Id: Idd0511f676ba74936bf1c00be4feb6656cb977af
Signed-off-by: Roi Dayan <roid@mellanox.com>

tc_tests_common.sh
@@ -313,7 +313,12 @@ function run_perf_test() {
         fail "Empty result"
     fi
 
+    local input_res
     if [ -f "$input_file" ]; then
+        input_res=`cat $input_file`
+    fi
+
+    if [ -n "$input_res"]; then
         key_val_to_array current < <(echo -e "$res")
         key_val_to_array goal < "$input_file"
         check_test_results goal current

commit cd2b01ebb00dd32c56d4dae390e42d94989e1562

tc_tests_common.sh: Fix input file check
Recent fix for input file check is missing a space before closing bracket
which results bash error and causes test to ignore the file name. Add the
missing space.

Fixes: ff4588d10564 ("tc_tests_common: Fix checking input_file")
Change-Id: Ie56415d7085b1c3428ec3db3f9e12027e2061472
Signed-off-by: Vlad Buslov <vladbu@mellanox.com>

tc_tests_common.sh
@@ -318,7 +318,7 @@ function run_perf_test() {
         input_res=`cat $input_file`
     fi
 
-    if [ -n "$input_res"]; then
+    if [ -n "$input_res" ]; then
         key_val_to_array current < <(echo -e "$res")
         key_val_to_array goal < "$input_file"
         check_test_results goal current