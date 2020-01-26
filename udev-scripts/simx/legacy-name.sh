bcc487bef00f9eb23af2cc6817efd7c0731968ad
Roi Dayan <roid@mellanox.com>
2020-01-26 13:55:40 +0200

    udev-scripts/simx/legacy-name.sh: fix compare
    Change-Id: I7082ad1e73ce74847d62c25e0acf9cf599007357
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- udev-scripts/simx/legacy-name.sh
+++ udev-scripts/simx/legacy-name.sh
@@ -4,12 +4,12 @@ if [ "$ID_NET_DRIVER" != "mlx5_core" ]; then
     exit 0
 fi
 
-if [ -z "$ID_NET_NAME_SLOT" ]; then
+if [ -n "$ID_NET_NAME_SLOT" ]; then
     echo NAME="${ID_NET_NAME_SLOT%%np[[:digit:]]}"
     exit 0
 fi
 
-if [ -z "$ID_NET_NAME_PATH" ]; then
+if [ -n "$ID_NET_NAME_PATH" ]; then
     echo NAME="${ID_NET_NAME_PATH%%np[[:digit:]]}"
     exit 0
 fi
