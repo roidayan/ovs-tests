e90c8eb1ab915cd5002daea06be3e9e414990d97
Roi Dayan <roid@mellanox.com>
2020-01-29 09:59:09 +0200

    common.sh: CHange uplink mode is not in background like eswitch mode
    So sleep is not needed.
    
    Change-Id: Ib9be847dac3299de066c19c8dba862c33a222a6c
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- common.sh
+++ common.sh
@@ -612,7 +612,6 @@ function set_uplink_rep_mode_nic_netdev() {
 
     if [ "$devlink_compat" = 1 ]; then
         echo nic_netdev > `devlink_compat_dir $nic`/uplink_rep_mode || fail "Failed to set mode nic_netdev"
-        sleep 3
         return
     fi
 
@@ -626,7 +625,6 @@ function set_uplink_rep_mode_new_netdev() {
 
     if [ "$devlink_compat" = 1 ]; then
         echo new_netdev > `devlink_compat_dir $nic`/uplink_rep_mode || fail "Failed to set mode new_netdev"
-        sleep 3
         return
     fi
 
