3bdd63d949ad80c7e6094995466a78d4ceac57c3
Roi Dayan <roid@mellanox.com>
2020-01-30 16:45:02 +0200

    udev-scripts/simx/legacy-name.sh: fix for enpX interfaces
    Change-Id: I9435e034c8407e64059c1964c411a220b10bf183
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- udev-scripts/simx/legacy-name.sh
+++ udev-scripts/simx/legacy-name.sh
@@ -13,8 +13,9 @@ else
     NAME=${ID_NET_NAME}
 fi
 
+NAME=${NAME%%np[[:digit:]]}
 # strip npX even from middle of the name.
 # e.g. new kernels have vf name as ens0f0np1vf0
-NAME=`echo $NAME | sed 's/np[0-9]\+//'`
+NAME=`echo $NAME | sed 's/np[0-9]\+vf/vf/'`
 echo NAME=$NAME
 exit 0
