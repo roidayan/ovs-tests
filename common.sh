roidayan/ovs-tests

commit 52504c76c380fd891415c48a5b286f23b6566780

common.sh: Add support for DPDK device whitelist
Change-Id: I32298594ddee3c6fe8bc70a94907bf3417142137
Signed-off-by: Salem Sol <salems@nvidia.com>

common.sh
@@ -1217,8 +1217,13 @@ function check_dpdk_init() {
 
     if [ "$a" != "$want" ]; then
         warn "OVS reset dpdk-init=$want"
-        [ "$want" == "true" ] && ovs-vsctl set Open_vSwitch . other_config:dpdk-init=true
-        [ "$want" == "" ] && ovs-vsctl remove Open_vSwitch . other_config dpdk-init
+        if [ "$want" == "true" ]; then
+           ovs-vsctl set Open_vSwitch . other_config:dpdk-init=true
+           ovs-vsctl set Open_vSwitch . other_config:dpdk-extra="-w $PCI,representor=[0,1],dv_xmeta_en=1"
+        else
+           ovs-vsctl remove Open_vSwitch . other_config dpdk-init
+           ovs-vsctl remove Open_vSwitch . other_config dpdk-extra
+        fi
         stop_openvswitch
         service_ovs start
     fi

commit 137095b30c00c510f0863c61c02b33e824932537

common.sh: Fix DPDK device whitelist
In some cases dpdk-extra is deleted from ovs other_config leaving
dpdk-init=true, thus skipping adding dpdk-extra again, add a check
for dpdk-exta if it exists and configure ovs other_config in case one of
them is missing.

Change-Id: I9f0fc09086d197339fadd78d87974aefbe563f1f
Fixes: 52504c76c380 ("common.sh: Add support for DPDK device whitelist")
Signed-off-by: Salem Sol <salems@nvidia.com>

common.sh
@@ -1215,6 +1215,7 @@ function check_ovs_settings() {
 
 function check_dpdk_init() {
     local a
+    local b
     local want=""
 
     if [ "${DPDK}" == 1 ]; then
@@ -1222,8 +1223,9 @@ function check_dpdk_init() {
     fi
 
     a=`ovs-vsctl get Open_vSwitch . other_config:dpdk-init 2>/dev/null | tr -d '"'`
+    b=`ovs-vsctl get Open_vSwitch . other_config:dpdk-extra 2>/dev/null | tr -d '"'`
 
-    if [ "$a" != "$want" ]; then
+    if [ "$a" != "$want" ] || [ "$b" == "" ]; then
         warn "OVS reset dpdk-init=$want"
         if [ "$want" == "true" ]; then
            ovs-vsctl set Open_vSwitch . other_config:dpdk-init=true