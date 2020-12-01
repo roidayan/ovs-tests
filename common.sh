roidayan/ovs-tests

commit 137095b30c00c510f0863c61c02b33e824932537

common.sh: Fix DPDK device whitelist
In some cases dpdk-extra is deleted from ovs other_config leaving
dpdk-init=true, thus skipping adding dpdk-extra again, add a check
for dpdk-exta if it exists and configure ovs other_config in case one of
them is missing.

Change-Id: I9f0fc09086d197339fadd78d87974aefbe563f1f
Fixes: 52504c76c380 ("common.sh: Add support for DPDK device whitelist")
Signed-off-by: Salem Sol <salems@nvidia.com>

common.sh
@@ -1215,6 +1215,7 @@ function check_ovs_settings() {
 
 function check_dpdk_init() {
     local a
+    local b
     local want=""
 
     if [ "${DPDK}" == 1 ]; then
@@ -1222,8 +1223,9 @@ function check_dpdk_init() {
     fi
 
     a=`ovs-vsctl get Open_vSwitch . other_config:dpdk-init 2>/dev/null | tr -d '"'`
+    b=`ovs-vsctl get Open_vSwitch . other_config:dpdk-extra 2>/dev/null | tr -d '"'`
 
-    if [ "$a" != "$want" ]; then
+    if [ "$a" != "$want" ] || [ "$b" == "" ]; then
         warn "OVS reset dpdk-init=$want"
         if [ "$want" == "true" ]; then
            ovs-vsctl set Open_vSwitch . other_config:dpdk-init=true

commit b2425362768b91bb326c35e7166506c1fac51a1b

common.sh: Fix dpdk init happening even if not needed
dpdk init should be done if current status is different
or dpdk extra is different and dpdk init set to true.

Fixes: 137095b30c00 ("common.sh: Fix DPDK device whitelist")
Change-Id: I0055ee1f91ccd7695a24e7ff6d262598b5b3ba31
Signed-off-by: Roi Dayan <roid@nvidia.com>

common.sh
@@ -1214,22 +1214,26 @@ function check_ovs_settings() {
 }
 
 function check_dpdk_init() {
-    local a
-    local b
+    local force=0
     local want=""
+    local want_extra="-w $PCI,representor=[0,1],dv_xmeta_en=1"
 
     if [ "${DPDK}" == 1 ]; then
         want="true"
     fi
 
-    a=`ovs-vsctl get Open_vSwitch . other_config:dpdk-init 2>/dev/null | tr -d '"'`
-    b=`ovs-vsctl get Open_vSwitch . other_config:dpdk-extra 2>/dev/null | tr -d '"'`
+    local init1=`ovs-vsctl get Open_vSwitch . other_config:dpdk-init 2>/dev/null | tr -d '"'`
+    local extra1=`ovs-vsctl get Open_vSwitch . other_config:dpdk-extra 2>/dev/null | tr -d '"'`
 
-    if [ "$a" != "$want" ] || [ "$b" == "" ]; then
+    if [ "$want" == "true" ] && [ "$want_extra" != "$extra1" ]; then
+        force=1
+    fi
+
+    if [ "$init1" != "$want" ] || [ "$force" == 1 ]; then
         warn "OVS reset dpdk-init=$want"
         if [ "$want" == "true" ]; then
            ovs-vsctl set Open_vSwitch . other_config:dpdk-init=true
-           ovs-vsctl set Open_vSwitch . other_config:dpdk-extra="-w $PCI,representor=[0,1],dv_xmeta_en=1"
+           ovs-vsctl set Open_vSwitch . other_config:dpdk-extra="$want_extra"
         else
            ovs-vsctl remove Open_vSwitch . other_config dpdk-init
            ovs-vsctl remove Open_vSwitch . other_config dpdk-extra