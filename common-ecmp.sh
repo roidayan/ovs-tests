roidayan/ovs-tests

commit 550c730c330c9f3c18e5410d27622c2470dca2f5

common-ecmp.sh: Sleep 0.5s before checking nics exists
If port2 was in switchdev and common.sh disabled it in the start
of the test it will take half a second for the interface to appear again.
This is if nic and uplink rep are not the same netdev (uplink rep redesign feature).
Usually tests do stuff before checking for nics. like change mode and that function
wait for nics so the sleep only needed the the ecmp tests that right away check for nics.

Change-Id: Id71f6f1d03eeba1fff23549fa9a4c4289b7f1286
Signed-off-by: Roi Dayan <roid@nvidia.com>

common-ecmp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sleep 0.5
 require_interfaces NIC NIC2
 require_module dummy
 

commit 07a3a57807455a573f92af2ea23282c23803bea1

common.sh: Sleep 0.5s when configuring sriov
We have more tests that check nic2 right when the test start.

Fixes: 550c730c330c ("common-ecmp.sh: Sleep 0.5s before checking nics exists")
Change-Id: Idcba42d63780a2370fa120da5b541231e8823f25
Signed-off-by: Roi Dayan <roid@nvidia.com>

common-ecmp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sleep 0.5
 require_interfaces NIC NIC2
 require_module dummy
 

common.sh
@@ -908,6 +908,7 @@ function config_sriov() {
         echo 0 > $numvfs
     fi
     echo $num > $numvfs || fail "Failed to config $num VFs on $nic"
+    sleep 0.5
     udevadm trigger -c add -s net &>/dev/null
 }
 